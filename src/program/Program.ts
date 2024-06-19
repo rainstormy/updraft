9c4a0a5f96e415cb81beaf3dfc7dc4f508c96017
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2024-06-19 21:50:48 +0200

    Inline configuration parsing into main program

--- src/program/Program.ts
+++ src/program/Program.ts
@@ -1,30 +1,67 @@
-import { getConfigurationFromArgs } from "+program/Configuration"
 import { invalidConfigurationProgram } from "+program/InvalidConfigurationProgram/InvalidConfigurationProgram"
 import { promotionProgram } from "+program/PromotionProgram/PromotionProgram"
 import { toolVersionProgram } from "+program/ToolVersionProgram/ToolVersionProgram"
 import { usageInstructionsProgram } from "+program/UsageInstructionsProgram/UsageInstructionsProgram"
-import type { ExitCode } from "+utilities/ErrorUtilities"
+import { parseArgs } from "+utilities/ArgsUtilities"
+import { type ExitCode, assertError } from "+utilities/ErrorUtilities"
+import { isSemanticVersionString } from "+utilities/StringUtilities"
 
 export async function mainProgram(args: Array<string>): Promise<ExitCode> {
-	const configuration = getConfigurationFromArgs(args)
+	try {
+		const parsedArgs = parseArgs(args, [
+			"--files",
+			"--help",
+			"--release-version",
+			"--version",
+		])
 
-	switch (configuration.type) {
-		case "help-screen": {
+		if (args.length === 0 || parsedArgs["--help"] !== null) {
 			return usageInstructionsProgram()
 		}
-
-		case "invalid": {
-			const { errorMessage } = configuration
-			return invalidConfigurationProgram(errorMessage)
-		}
-
-		case "release": {
-			const { filePatterns, releaseVersion } = configuration
-			return promotionProgram(filePatterns, releaseVersion)
-		}
-
-		case "tool-version": {
+		if (parsedArgs["--version"] !== null) {
 			return toolVersionProgram()
 		}
+
+		const rawReleaseVersions = parsedArgs["--release-version"]
+
+		if (rawReleaseVersions === null) {
+			return invalidConfigurationProgram("--release-version must be specified.")
+		}
+		if (rawReleaseVersions.length === 0) {
+			return invalidConfigurationProgram(
+				"--release-version must specify a value.",
+			)
+		}
+		if (rawReleaseVersions.length > 1) {
+			return invalidConfigurationProgram(
+				"--release-version must not specify more than one value.",
+			)
+		}
+
+		const rawReleaseVersion = rawReleaseVersions[0]
+
+		const releaseVersion = rawReleaseVersion.startsWith("v")
+			? rawReleaseVersion.slice(1)
+			: rawReleaseVersion
+
+		if (!isSemanticVersionString(releaseVersion)) {
+			return invalidConfigurationProgram(
+				`--release-version has an invalid value '${rawReleaseVersion}'.`,
+			)
+		}
+
+		const files = parsedArgs["--files"]
+
+		if (files === null) {
+			return invalidConfigurationProgram("--files must be specified.")
+		}
+		if (files.length === 0) {
+			return invalidConfigurationProgram("--files must specify a value.")
+		}
+
+		return promotionProgram(files, releaseVersion)
+	} catch (error) {
+		assertError(error)
+		return invalidConfigurationProgram(error.message)
 	}
 }
---
+++ src/utilities/ArgsUtilities.ts
@@ -0,0 +1,33 @@
+export function parseArgs<Option extends string>(
+	args: Array<string>,
+	options: Array<Option>,
+): Record<Option, Array<string> | null> {
+	const parsedArgs = Object.fromEntries(
+		options.map((option) => [option, null]),
+	) as Record<Option, Array<string> | null>
+
+	let currentOption: Option | null = null
+
+	for (const arg of args) {
+		if (isOption(arg)) {
+			if (parsedArgs[arg] !== null) {
+				throw new Error(`${arg} must be specified only once.`)
+			}
+			currentOption = arg
+			parsedArgs[currentOption] = [] as Array<string>
+			continue
+		}
+		if (arg.startsWith("-")) {
+			throw new Error(`Unknown option '${arg}'.`)
+		}
+		if (currentOption !== null) {
+			parsedArgs[currentOption]?.push(arg)
+		}
+	}
+
+	return parsedArgs
+
+	function isOption(arg: string): arg is Option {
+		return (options as Array<string>).includes(arg)
+	}
+}
