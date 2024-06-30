986a6f6432f3bfdc4e73590fcbd12a012ff3321f
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2024-06-30 20:25:13 +0200

    Reduce complexity in args parsing

--- src/program/Program.ts
+++ src/program/Program.ts
@@ -2,79 +2,68 @@ import { invalidConfigurationProgram } from "+program/InvalidConfigurationProgra
 import { promotionProgram } from "+program/PromotionProgram/PromotionProgram"
 import { toolVersionProgram } from "+program/ToolVersionProgram/ToolVersionProgram"
 import { usageInstructionsProgram } from "+program/UsageInstructionsProgram/UsageInstructionsProgram"
-import { parseArgs } from "+utilities/ArgsUtilities"
+import { defineOptions, parseArgs } from "+utilities/ArgsUtilities"
 import { type ExitCode, assertError } from "+utilities/ErrorUtilities"
-import { extractSemanticVersionString } from "+utilities/StringUtilities"
+import {
+	extractSemanticVersionString,
+	isPrerelease,
+} from "+utilities/StringUtilities"
+
+const schema = defineOptions({
+	"--files": {
+		args: { min: 1 },
+	},
+	"--prerelease-files": {
+		args: { min: 1 },
+	},
+	"--release-files": {
+		args: { min: 1 },
+	},
+	"--release-version": {
+		required: true,
+		args: { min: 1, max: 1 },
+	},
+})
 
 export async function mainProgram(args: Array<string>): Promise<ExitCode> {
+	if (args.length === 0 || args.includes("--help")) {
+		return usageInstructionsProgram()
+	}
+	if (args.includes("--version")) {
+		return toolVersionProgram()
+	}
+
+	let parsedArgs: Record<keyof typeof schema, Array<string> | undefined>
+
 	try {
-		const parsedArgs = parseArgs(args, [
-			"--files",
-			"--help",
-			"--prerelease-files",
-			"--release-files",
-			"--release-version",
-			"--version",
-		])
-
-		if (args.length === 0 || parsedArgs["--help"] !== null) {
-			return usageInstructionsProgram()
-		}
-		if (parsedArgs["--version"] !== null) {
-			return toolVersionProgram()
-		}
-
-		const rawReleaseVersions = parsedArgs["--release-version"]
-
-		if (rawReleaseVersions === null) {
-			return invalidConfigurationProgram("--release-version is required.")
-		}
-		if (rawReleaseVersions.length === 0) {
-			return invalidConfigurationProgram("--release-version requires a value.")
-		}
-		if (rawReleaseVersions.length > 1) {
-			return invalidConfigurationProgram(
-				"--release-version cannot have more than one value.",
-			)
-		}
-
-		const releaseVersion = extractSemanticVersionString(rawReleaseVersions[0])
-
-		if (releaseVersion === null) {
-			return invalidConfigurationProgram(
-				`--release-version has an invalid value '${rawReleaseVersions[0]}'.`,
-			)
-		}
-
-		const files = parsedArgs["--files"]
-		if (files?.length === 0) {
-			return invalidConfigurationProgram("--files requires a value.")
-		}
-
-		const releaseFiles = parsedArgs["--release-files"]
-		if (releaseFiles?.length === 0) {
-			return invalidConfigurationProgram("--release-files requires a value.")
-		}
-
-		const prereleaseFiles = parsedArgs["--prerelease-files"]
-		if (prereleaseFiles?.length === 0) {
-			return invalidConfigurationProgram("--prerelease-files requires a value.")
-		}
-
-		if (files === null && releaseFiles === null && prereleaseFiles === null) {
-			return invalidConfigurationProgram(
-				"--files, --release-files, or --prerelease-files is required.",
-			)
-		}
-
-		return promotionProgram(
-			files ?? [],
-			prereleaseFiles ?? [],
-			releaseFiles ?? [],
-			releaseVersion,
-		)
+		parsedArgs = parseArgs(schema, args)
 	} catch (error) {
 		assertError(error)
 		return invalidConfigurationProgram(error.message)
 	}
+
+	const files = parsedArgs["--files"] ?? []
+	const prereleaseFiles = parsedArgs["--prerelease-files"] ?? []
+	const releaseFiles = parsedArgs["--release-files"] ?? []
+	const releaseVersion = parsedArgs["--release-version"]?.[0] ?? ""
+
+	if (files.length + prereleaseFiles.length + releaseFiles.length === 0) {
+		return invalidConfigurationProgram(
+			"--files, --release-files, or --prerelease-files is required.",
+		)
+	}
+
+	const semanticReleaseVersion = extractSemanticVersionString(releaseVersion)
+
+	if (semanticReleaseVersion === null) {
+		return invalidConfigurationProgram(
+			`--release-version has an invalid value '${releaseVersion}'.`,
+		)
+	}
+
+	const filePatterns = isPrerelease(semanticReleaseVersion)
+		? [...files, ...prereleaseFiles]
+		: [...files, ...releaseFiles]
+
+	return promotionProgram(filePatterns, semanticReleaseVersion)
 }
