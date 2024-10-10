92c3a40f6057b233f960710f44202ab52cff054d
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2024-10-10 22:25:22 +0200

    Eliminate types nested in namespaces

--- src/utilities/types/DateString.ts
+++ src/utilities/types/DateString.ts
@@ -1,8 +1,2 @@
 export type DateString =
-	`${DateString.Year}-${DateString.Month}-${DateString.Day}`
-
-export namespace DateString {
-	export type Year = `${number}${number}${number}${number}`
-	export type Month = `${number}${number}`
-	export type Day = `${number}${number}`
-}
+	`${number}${number}${number}${number}-${number}${number}-${number}${number}`
--- src/utilities/types/SemanticVersionString.ts
+++ src/utilities/types/SemanticVersionString.ts
@@ -1,16 +1,10 @@
 import { isSequentialUpgrade } from "+utilities/types/ComparableSemanticVersionString"
 
 export type SemanticVersionString =
-	| `${SemanticVersionString.MajorMinorPatch}`
-	| `${SemanticVersionString.MajorMinorPatch}${SemanticVersionString.Build}`
-	| `${SemanticVersionString.MajorMinorPatch}${SemanticVersionString.Prerelease}`
-	| `${SemanticVersionString.MajorMinorPatch}${SemanticVersionString.Prerelease}${SemanticVersionString.Build}`
-
-export namespace SemanticVersionString {
-	export type MajorMinorPatch = `${number}.${number}.${number}`
-	export type Prerelease = `-${string}`
-	export type Build = `+${string}`
-}
+	| `${number}.${number}.${number}`
+	| `${number}.${number}.${number}+${string}`
+	| `${number}.${number}.${number}-${string}`
+	| `${number}.${number}.${number}-${string}+${string}`
 
 export const semanticVersionRegex =
 	/(?<fullVersion>(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?<prerelease>-(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(?<build>\+[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*)?)/
