19bab3661eec1a06d15e312e29d476cd7fccc2be
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2024-11-22 23:16:59 +0100

    Fix Vitest types

--- src/utilities/ModuleMock.ts
+++ src/utilities/ModuleMock.ts
@@ -3,8 +3,5 @@ import type { Mock } from "vitest"
 export type ModuleMock<
 	Module extends Record<string, (...params: never) => unknown>,
 > = {
-	[Name in keyof Module]: Mock<
-		Parameters<Module[Name]>,
-		ReturnType<Module[Name]>
-	>
+	[Name in keyof Module]: Mock<Module[Name]>
 }
